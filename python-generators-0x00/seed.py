7f984f411ba5abc5426fe36f93ae3102ced37858
mazikinzy <mazikinzy@outlook.com>
2025-12-17 06:36:00 +0000

    Generator commit

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -2,7 +2,9 @@
 
 import sqlite3
 
-connection = "ALX_prodev.sql"
+data_base = """
+    CREATE DATABASE IF NOT EXISTS ALX_prodev;
+"""
 table = """
 CREATE TABLE IF NOT EXISTS user_data (
     user_id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -11,15 +13,37 @@ CREATE TABLE IF NOT EXISTS user_data (
     age DECIMAL NOT NULL
 );
 """
+table_data = 'C:\\Users\\mazik\\Desktop\\Tech\\user_data.csv'
 
-def create_database(connection):
-    conn = sqlite3.connect(connection)
+def connect_db():
+    conn = sqlite3.connect(data_base)
     cursor = conn.cursor()
     return conn, cursor
 
 
-def create_insert_table(cursor, k):
-    cursor.execute(k)
+def create_database(connection):
+    conn = sqlite3.connect(connection)
+    conn.close()
+
+
+def connect_to_prodev():
+    conn = sqlite3.connect("ALX_prodev")
+    cursor = conn.cursor()
+    return conn, cursor
+
+
+def create_table(connection):
+    conn = sqlite3.connect("ALX_prodev")
+    cursor = conn.cursor()
+    cursor.execute(table)
+
+
+def insert_data(connection, data):
+    connection = create_table(connection)
+    data = table_data
+    cursor.execute(
+    "INSERT INTO connection (user_name, email, age) VALUES (data)"
+)
 
 
 def fetch_all(cursor, rows):
