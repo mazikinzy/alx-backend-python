b647b5288c12dbe191fca61a6c8cbce4cc9c60b5
Kingsley Orji <mazikinzy@outlook.com>
2026-02-18 03:13:13 +0000

    Fix database connection and SQL syntax issues

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -2,20 +2,19 @@
 
 import sqlite3
 
-data = "user_data.csv"
+data = "python-generators-0x00/user_data.csv"
+connection = sqlite3.connect('ALX_prodev')
 
 def connect_db():
     connection = sqlite3.connect('ALX_prodev')
-    connection.close()
 
 
 def create_database():
     connection = sqlite3.connect('ALX_prodev')
     cursor = connection.cursor()
-    cursor.execute("CREATE DATABASE IF NOT EXISTS ALX_prodev;")
+    cursor.execute("CREATE DATABASE IF NOT EXISTS ALX_prodev")
     
-    cursor.commit()
-    cursor.close()
+    connection.commit()
 
 
 def connect_to_prodev():
@@ -26,31 +25,31 @@ def connect_to_prodev():
 def create_table():
     connection = sqlite3.connect('ALX_prodev')
     cursor = connnnection.cursor()
-    cursor.execute(CREATE TABLE IF NOT EXISTS user_data (
+    cursor.execute("CREATE TABLE IF NOT EXISTS user_data(
         user_id INTEGER AUTO_INCREMENT PRIMARY KEY,
         user_name VARCHAR NOT NULL,
         email VARCHAR NOT NULL,
         age DECIMAL NOT NULL
-        )
+        )"
     )
     
-    cursor.commit()
-    cursor.close()
+    connection.commit()
 
 
-def insert_data():
+def insert_data(data):
     connection = sqlite3.connect('ALx_prodev')
     cursor = connection.cursor()
-    cursor.executemany("INSERT INTO user_data (user_name, email, age) VALUES (?, ?, ?)", data)
+    cursor.executemany("INSERT INTO user_data(user_name, email, age) VALUES (?, ?, ?)", data)
 
-    conn.commit()
-    conn.close()
+    connection.commit()
 
 
 def disconnect():
-    conn.close()
+    connection = sqlite3.connect('ALx_prodev')
+    connection.close()
 
 
 def commit():
-    conn.commit()
+    connection = sqlite3.connect('ALx_prodev')
+    connection.commit()
 
